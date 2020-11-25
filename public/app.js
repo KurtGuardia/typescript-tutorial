5482ccaab3c1f232fb9fd8d38222690d3514eb77
Kurt Guardia <kurtguardia@gmail.com>
2020-11-25 09:03:20 +0100

    created the list template class

--- public/app.js
+++ public/app.js
@@ -7,14 +7,12 @@ const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let doc;
-    if (type.value === 'invoice') {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
-    }
-    else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
-    }
-    console.log(doc);
+  e.preventDefault();
+  let doc;
+  if (type.value === 'invoice') {
+    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+  } else {
+    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+  }
+  console.log(doc);
 });
-console.log(type);

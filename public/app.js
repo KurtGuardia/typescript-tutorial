6e88029add5891ace5e338c774b3217ebea3d324
Kurt Guardia <kurtguardia@gmail.com>
2020-12-03 09:09:22 +0100

    implemented rendering list to UI

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/listTemplate.js';
 import { Payment } from './classes/Payment.js';
 const form = document.querySelector('.new-item-form');
 // inputs
@@ -6,13 +7,17 @@ const type = document.querySelector('#type');
 const toFrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let doc;
-  if (type.value === 'invoice') {
-    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
-  } else {
-    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
-  }
-  console.log(doc);
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
 });
